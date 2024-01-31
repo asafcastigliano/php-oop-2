d0cbf3592ee8a31bf0005c8d8fabd9bdaacf89fd
asafcastigliano <asafcastigliano@gmail.com>
2024-01-31 16:39:08 +0100

    agginte classi con ereditarietà e array $prodotto.

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 
     class Prodotto {
         public $nome;
-        public $prezzo;
         public $immagine;
         public $categoria;
 
-        public function __construct($nome, $prezzo, $immagine, $categoria) {
+        public function __construct($nome, $immagine, $categoria) {
             $this->nome = $nome;
-            $this->prezzo = $prezzo;
             $this->immagine = $immagine;
             $this->categoria = $categoria;
         }
@@ -16,10 +14,32 @@
         public function mostra() {
             echo "<img src='{$this->immagine}' alt='{$this->nome}'>";
             echo "<h3>{$this->nome}</h3>";
-            echo "<p>Prezzo: {$this->prezzo}€</p>";
             echo "<p>Categoria: {$this->categoria}</p>";
         }
 
     }
+
+    class Cibo extends Prodotto {
+        public $tipo = 'Cibo';
+    }
+    
+    class Gioco extends Prodotto {
+        public $tipo = 'Gioco';
+    }
+    
+    class Accessorio extends Prodotto {
+        public $tipo = 'Accessorio';
+    }
+
+    $prodotto = [
+        new Cibo("Royal Canin Mini Adult", "https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg", "Cane"),
+        new Cibo("Almo Nature Holistic Maintenance Medium Large Tonno e Riso", "https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg", "Cane"),
+        new Cibo("Almo Nature Cat Daily Lattina", "https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg", "Gatto"),
+        new Cibo("Mangime per Pesci Guppy in Fiocchi", "https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg", "Pesci"),
+        new Accessorio("Voliera Wilma in Legno", "https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg", "Uccelli"),
+        new Accessorio("Cartucce Filtranti per Filtro EasyCrystal", "https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg", "Pesci"),
+        new Gioco("Kong Classic", "https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg", "Cane"),
+        new Gioco("Topini di peluche Trixie", "https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg", "Gatto")
+    ]
 
 ?>
